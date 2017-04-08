bdc630ce66b322be9aee6d354235c681cfba7be0
Peter Hong <peterhong47@gmail.com>
2017-04-07 17:49:17 -0700

    two players, one with gamepad and one with keyboard

--- Assets/Scripts/Game_Manager.cs
+++ Assets/Scripts/Game_Manager.cs
@@ -1,17 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Game_Manager : MonoBehaviour {
-    public int num_of_players;
     public Player[] players;
     private int turn = 0;
-	void Start () {
-        players = new Player[num_of_players];
-	}
+    private int current_turn = 0;
+
+    void Awake() {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+	void Start() {
+        players[current_turn].turn = true;
+        players[current_turn].gameObject.SetActive(true);
+    }
 	
 	void Update () {
-		
+		if(players[current_turn].turn == false) {
+            current_turn++;
+            if (current_turn == players.Length) {
+                current_turn = 0;
+                SceneManager.LoadScene(1);
+            }
+            players[current_turn].turn = true;
+            players[current_turn].gameObject.SetActive(true);
+        }
 	}
-
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,52 +6,63 @@ public class Player : MonoBehaviour {
     public GameObject dice;             //instantiate when able to jump
     public float thrust = 95f;
     public int player_num;
+    public bool turn;          //for being able to move available steps
+    public int steps_can_move;
 
     private const float step = .25f;
     private Rigidbody rb;
-    private bool turn = false;          //for being able to move available steps
     private bool can_jump = true;       //for hitting the dice
-    private int steps_can_move = 0;
     private bool dice_made = false;
     private Dice current_dice;
     private bool input_clear = false;
+    private bool can_move = false;
 
+    void Awake() {
+        DontDestroyOnLoad(transform.gameObject);
+    }
 
     void Start () {
         rb = GetComponent<Rigidbody>();
+        steps_can_move = 0;
+        foreach(string e in Input.GetJoystickNames()) {
+            Debug.Log(e);
+        }
     }
 	
 	void Update () {
-        if (turn && steps_can_move > 0) {
+        if (can_move && steps_can_move > 0) {
             movement();
         }
         //means turn to dice jump
-        if (can_jump) {
+        if (turn && can_jump) {
             if (!dice_made) {
                 GameObject inst = Instantiate(dice) as GameObject;
                 Vector3 pos = new Vector3(transform.position.x, transform.position.y + .75f, transform.position.z);
                 inst.transform.position = pos;
+                inst.transform.rotation = this.transform.rotation;
                 dice_made = true;
             }
             //need to add floor check so can't repeatedly jump
-            if (Input.GetButtonDown("Jump")) {
+            if (Input.GetButtonDown("Jump" + player_num)) {
                 rb.AddForce(transform.up * thrust);
                 can_jump = false;
-                turn = true;
+                can_move = true;
+                
             }
         }
     }
 
     private void movement() {
-        if(Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0) {
+        Debug.Log(player_num);
+        if (Input.GetAxis("Horizontal" + player_num) == 0 && Input.GetAxis("Vertical" + player_num) == 0) {
             input_clear = true;
         }
         if (input_clear) {
-            float vert = Input.GetAxis("Vertical");
-            float hori = Input.GetAxis("Horizontal");
+            float vert = Input.GetAxis("Vertical" + player_num);
+            float hori = Input.GetAxis("Horizontal" + player_num);
+            Debug.Log(vert);
             if (vert > 0) {
                 input_clear = false;
-                Debug.Log("up");
                 transform.Translate(Vector3.forward);
                 current_dice.transform.Translate(Vector3.forward);
                 steps_can_move--;
@@ -59,31 +70,26 @@ public class Player : MonoBehaviour {
             }
             else if(vert < 0) {
                 input_clear = false;
-                Debug.Log("down");
                 transform.Rotate(180f * Vector3.up);
                 current_dice.transform.Rotate(180f * Vector3.up);
             }
             else if (hori < 0) {
                 input_clear = false;
-                Debug.Log("left");
                 transform.Rotate(90f * -Vector3.up);
                 current_dice.transform.Rotate(90f * -Vector3.up);
             }
             else if (hori > 0) {
                 input_clear = false;
-                Debug.Log("right");
                 transform.Rotate(90f * Vector3.up);
                 current_dice.transform.Rotate(90f * Vector3.up);
             }
         }
 
-        if (steps_can_move <= 0)
+        if (steps_can_move <= 0) {
             turn = false;
-    }
-
-    public void set_turn(int steps) {
-        turn = true;
-        steps_can_move = steps;
+            dice_made = can_move = false;
+            can_jump = true;
+        }
     }
 
     void OnTriggerEnter(Collider collide) {
