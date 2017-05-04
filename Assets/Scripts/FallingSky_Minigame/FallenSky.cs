796b8683f1157bc767ba7da7914dd38c0f33dbf3
Michael <mtzimmerman91@gmail.com>
2017-05-04 16:14:42 -0700

    objects to dodge setup, need to figure out how to handle player spawning

---
+++ Assets/Scripts/FallingSky_Minigame/FallenSky.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallenSky : MonoBehaviour {
+    public int WaitForSecondsRealTime { get; private set; }
+
+    // Use this for initialization
+    void Start () {
+        DestroyObject(gameObject, 5);
+	}
+}
---
+++ Assets/Scripts/FallingSky_Minigame/FallenSkyController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallenSkyController : MonoBehaviour {
+
+    public GameObject target;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnObject", 2, 0.5f);
+    }
+
+    void SpawnObject()
+    {
+        float x = Random.Range(-2.5f, 2.5f);
+        float z = Random.Range(-4.0f, 2.0f);
+        Instantiate(target, new Vector3(x, 5, z), Quaternion.identity);
+    }
+}
