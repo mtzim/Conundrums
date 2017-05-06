0cd6379e910df9f842ee9fe0656ff5f5777f0eda
Michael <mtzimmerman91@gmail.com>
2017-05-05 19:05:59 -0700

    pause menu finished, need to pause game now when pause menu displays

---
+++ Assets/Scripts/FallingSky_Minigame/PauseMenuFS.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuFS : MonoBehaviour
+{
+
+    private bool toggle;
+    public GameObject pauseMenu;
+
+    // Use this for initialization
+    void Start()
+    {
+        toggle = true;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            resetPanels();
+        }
+    }
+
+    /*private void TogMouseLook(bool enable)
+    {
+        player.GetComponent<MovementSP>().enabled = enable;
+        player.GetComponentInChildren<CameraControllerSP>().enabled = enable;
+    }*/
+
+    public void ClickedButton()
+    {
+        Cursor.visible = toggle;
+        toggle = !toggle;
+        //TogMouseLook(toggle);
+        pauseMenu.SetActive(!toggle);        
+    }
+
+    private void resetPanels()
+    {
+        if (pauseMenu.transform.GetChild(0).gameObject.activeSelf == true)
+        {
+            pauseMenu.SetActive(toggle);
+            Cursor.visible = toggle;
+            toggle = !toggle;
+            //TogMouseLook(toggle);
+        }
+        else
+        {
+            pauseMenu.transform.GetChild(1).gameObject.SetActive(false);
+            pauseMenu.transform.GetChild(2).gameObject.SetActive(false);
+            pauseMenu.transform.GetChild(3).gameObject.SetActive(false);
+            pauseMenu.transform.GetChild(0).gameObject.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/FallingSky_Minigame/StartMiniGame.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMiniGame : MonoBehaviour {
+
+    public GameObject player1;
+    public GameObject player2;
+    public GameObject player3;
+    public GameObject player4;
+
+    public void StartGame()
+    {
+        Camera.main.GetComponent<FallenSkyController>().enabled = true;
+        player1.GetComponent<PlayerController>().enabled = true;
+        player2.GetComponent<PlayerController>().enabled = true;
+        player3.GetComponent<PlayerController>().enabled = true;
+        player4.GetComponent<PlayerController>().enabled = true;
+        gameObject.GetComponent<PauseMenuFS>().enabled = true;
+    }
+}
