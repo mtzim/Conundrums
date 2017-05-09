d87a5d315298bd58f2ca67d54ca5b574dda1841c
Justin Olaso <Justin Olaso>
2017-05-09 13:11:57 -0700

    Created Pachinko Chasers Minigame. Added Sword Swing Animation to PlayerController script. Revamped Fallen Sky with updated characters and animations.

---
+++ Assets/Scripts/DeathByPlinko/SwordSmack.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordSmack : MonoBehaviour
+{
+    public float bounce_force;
+    public GameObject ownSelf;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player") && collision.gameObject.tag != ownSelf.gameObject.tag)
+        {
+            Debug.Log("hit " + collision.gameObject.tag);
+
+            Vector3 direction = collision.contacts[0].point - transform.position;
+            direction = direction.normalized;
+            collision.gameObject.GetComponent<Rigidbody>().AddForce(direction * bounce_force);
+        }
+    }
+}
