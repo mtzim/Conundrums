81c16d2f46078bf9fbd8a4d8ff9c89a173d98b92
Michael <mtzimmerman91@gmail.com>
2017-05-10 00:14:04 -0700

    choosing number of players from main menu reflects in board game now

---
+++ Assets/Scripts/NumPlayers.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumPlayers : MonoBehaviour {
+
+    [HideInInspector]
+    public int numberOfPlayers;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void setNumberOfPlayers(int numPlayers)
+    {
+        numberOfPlayers = numPlayers;
+    }
+}
